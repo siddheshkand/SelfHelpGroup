fb6ba49fec0a57c2237f05fab101736baa11b9a3
rahul <rshahshah2890@gmail.com>
2019-01-01 21:47:00 +0530

    Migrations

--- Home/forms.py
+++ Home/forms.py
@@ -7,4 +7,4 @@ class RegistrationForm(forms.ModelForm):
     class Meta:
         model = Member
         # fields = '__all__'
-        exclude = ['is_active']
+        exclude = ['is_active', 'user']
