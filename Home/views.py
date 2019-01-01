99575e4630ba72a121c94f4482fbcf096c791e23
rahul <rshahshah2890@gmail.com>
2019-01-01 21:34:42 +0530

    added registration froms.py

--- Home/views.py
+++ Home/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def login_user(request):
