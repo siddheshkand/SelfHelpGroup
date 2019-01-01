42a922897a7b572ada4bdbae200dd7813810e10c
siddheshkand <siddkand@gmail.com>
2019-01-01 19:55:39 +0530

    Home view fixed

--- SelfHelpGroup/settings.py
+++ SelfHelpGroup/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     # User Define
     'Collection.apps.CollectionConfig',
     'Loan.apps.LoanConfig',
+    'Home',
 ]
 
 MIDDLEWARE = [
--- SelfHelpGroup/urls.py
+++ SelfHelpGroup/urls.py
@@ -18,11 +18,11 @@ from django.contrib import admin
 from django.urls import path
 
 from SelfHelpGroup import settings
-from Collection import views
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', views.home)
+    path('home/', views.home),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
